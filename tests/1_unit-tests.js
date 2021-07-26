ef42e458dbb73292118ba139fb6c43d6465e032e
Jon <jdegnen@gmail.com>
2021-07-26 23:08:27 +0100

    test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -31,18 +31,18 @@ suite("UnitTests", () => {
 	// 	done();
 	// });
 
-	// test("Logic handles a puzzle string that is not 81 characters in length", function (done) {
-	// 	assert.throws(
-	// 		() => {
-	// 			solver.validate(
-	// 				"4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
-	// 			);
-	// 		},
-	// 		"Expected puzzle to be 81 characters long",
-	// 		"Incorrect puzzle length"
-	// 	);
-	// 	done();
-	// });
+	test("Logic handles a puzzle string that is not 81 characters in length", function (done) {
+		assert.throws(
+			() => {
+				solver.validate(
+					"4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+				);
+			},
+			"Expected puzzle to be 81 characters long",
+			"Incorrect puzzle length"
+		);
+		done();
+	});
 
 	test("Logic handles valid row placement", function (done) {
 		assert.deepEqual(
